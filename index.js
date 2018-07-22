a439a9c4949e1a74f615c1d09a8323836b5b12cf
warikjames <warikjames@users.noreply.github.com>
2018-07-23 07:52:48 +1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+/* 2 connected to a room*/
+var roomno = 1;
+io.on('connection', function (socket) {
+    //Increase roomno 2 clients are present in a room.
+    
+    if (io.nsps['/'].adapter.rooms["room-" + roomno] && io.nsps['/'].adapter.rooms["room-" + roomno].length > 1)
+        roomno++;
+    socket.join("room-" + roomno);
+    
+    //Send this event to everyone in the room.
+    io.sockets.in("room-" + roomno).emit('startmessage', "Connected to ToyCtrl Room#: " + roomno);
+    
+    socket.on('message1', function (txt) {
+        io.sockets.in("room-"+ roomno).emit('message1', txt);
+        //io.sockets.in("room-" + roomno).emit('message1', txt);
+    });
+    
+
+//    socket.join("edibleplastyc-toyctrl");
+//    io.sockets.in("edibleplastyc-toyctrl").emit('startmessage', "Welcome, you are now connecting to the plastyctoy ");
+//    socket.on('message1', function (txt) {
+//        io.sockets.in("edibleplastyc-toyctrl").emit('message1', txt);
+//    });
+
+    
+});
+
+/* Namespace */
+//var nsp = io.of('/ctrl');
+//nsp.on('connection', function (socket) {
+//        
+//    nsp.emit('message', 'Toy Connected');
+//
+//    socket.on('message', function(txt){
+//        nsp.emit('message', txt, socket.id);
+//    });
+//    
+//});
+
+
+///* default */
+//sio.on('connection', function(socket){
+//    
+//    sio.emit( 'greetings', 'Hello from the server: ' + socket.id , socket.id );
+//    
+//    //sio.emit('message',  'User: ' + socket.id + ' Connected');
+//    
+//    // listen for the 'message' event
+//    socket.on('message', function(txt){
+//        sio.emit('message', txt, socket.id);
+//    });
+//    
+//    socket.on('disconnect', function() {
+//      //console.log('Got disconnect!');
+//        sio.emit('message',  'User: ' + socket.id + ' Disconnected');
+//   });
+//    
+//    socket.on('message1', function(txt){
+//        sio.emit('message1', txt, socket.id);
+//    });
+//
+//});
+
+http.listen(port, function () {
+    console.log('listening on *:' + port);
+});
+
+/* --------------------------------  simple chat example above -----------*/
